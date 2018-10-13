20c11b7cc8ff46f91a3a35123655877b14dd1d27
Daniel Barclay <barclaysd@me.com>
2018-10-13 23:47:42 +0100

    Added renderer IPC and main process communication handlers

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+const electron = require('electron');
+const ipcMain = electron.ipcMain;
 const path = require('path');
+const util = require('util');
+const fs = require('fs');
 
 const {
     app,
     BrowserWindow
 } = require('electron');
+
+// allow fs.stat to use promises
+const stat = util.promisify(fs.stat)
 
 let mainWindow;
 
@@ -13,4 +20,24 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow();
     // load html file
     mainWindow.loadFile(htmlPath);
+})
+
+// listen for files event by browser process
+ipcMain.on('files', async (event, filesArr) => {
+    try {
+        // asynchronously load data for files
+        const data = await Promise.all(
+            filesArr.map(async ({
+                name,
+                pathName
+            }) => ({
+                ...await stat(pathName),
+                name,
+                pathName
+            }))
+        )
+        mainWindows.webContents.send('metadata', data)
+    } catch (error) {
+        mainWindow.webContents.send('metadata:error', error)
+    }
 })
--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,36 @@
+const ipc = require('electron').ipcRenderer;
+const util = require('util');
+const fs = require('fs');
+
 const submitListener = document
     .querySelector('form')
     .addEventListener('submit', (event) => {
         // prevent default behaviour that causes the page to refresh
         event.preventDefault();
 
-        const files = document.getElementById('filePicker').files;
-        console.log(files);
+        const files = [...document.getElementById('filePicker').files];
+        //     console.log(files)
+        // })
+        const filesFormatted = files.map(({
+            name,
+            path: pathName
+        }) => ({
+            name,
+            pathName
+        }))
+
+        // send data to the main process
+        ipc.send('files', filesFormatted);
     })
+
+// analyse metadata from main prcoess
+ipc.on('metadata', (event, metadata) => {
+    const pre = document.getElementById('data')
+
+    pre.innerText = JSON.stringify(metadata, null, 2)
+})
+
+// error handler from catch block in main process
+ipc.on('metadata:error', (event, error) => {
+    console.error(error);
+})
